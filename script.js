1b9a5aeed1df16cac727b40585245cc8082b43c9
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-10-04 17:52:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-function updateClock() {
-    const timerElement = document.getElementById('timer');
-    const now = new Date();
-    
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
-
-    timerElement.textContent = `${date} ${time}`;
+function updateClock(){
+	const timerElement = document.getElementById('timer');
+	const now = newDate();
+	const date = now.toLocaleDateString();
+	const time = now.toLocaleTimeString();
+	timeElement.textContent = `$(date)$(time)`;
 }
+setInterval(()=>{
+	
+}, 1000);
 
-// Update the clock every second (1000 milliseconds)
-setInterval(updateClock, 1000);
-
-// Initial call to display the time immediately
 updateClock();
