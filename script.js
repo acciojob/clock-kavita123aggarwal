3ea22fd2d426384d0b11ad41c7a2ad1b639a1ccd
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-10-04 17:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function updateClock() {
+    const timerElement = document.getElementById('timer');
+    const now = new Date();
+    
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+
+    timerElement.textContent = `${date} ${time}`;
+}
+
+// Update the clock every second (1000 milliseconds)
+setInterval(updateClock, 1000);
+
+// Initial call to display the time immediately
+updateClock();
